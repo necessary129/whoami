c3f86eac029d382b03366d7f38370d083ec2dc82
Shamil K <noteness@disroot.org>
2021-07-05 11:53:45 +0530

    don't output curl data

--- upload-to-ia.sh
+++ upload-to-ia.sh
@@ -3,4 +3,4 @@
 set -eu
 url=$(cat identities/url)
 
-curl -X POST "https://web.archive.org/save/$url" --data-urlencode "url=$url" --data-urlencode "capture_all=off"
+curl -X POST "https://web.archive.org/save/$url" --data-urlencode "url=$url" --data-urlencode "capture_all=off" > /dev/null
